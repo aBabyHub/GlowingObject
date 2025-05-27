288130879f8700f61912d61d43db5956cdfb7ba2
aBabyHub <28michael.kao@lwhs.org>
2025-05-27 11:59:55 -0700

    made the code work

--- code.py
+++ code.py
@@ -13,24 +13,28 @@ pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.7, auto_write=Fal
 mic_pin = board.A2
 mic = analogio.AnalogIn(mic_pin)
 
+previous_mic_value = 0
+
 color_value = mic.value/455
 mic_value_array = array.array('i', [0, 0])
 
-def calculate_mean(data_array):
-  if not data_array:
-    return 0
-  total = sum(data_array)
-  mean = total / len(data_array)
-  return mean
-
 while True:
-    if mic.value < 65025:
-        mic_value_array.append(int(mic.value/455))
-
-        COLOR = (int(mic.value/455), 0, 0)
-        color_value = int(mic.value/455)
+    mic_value = abs(mic.value)
+    
+    print("difference:")
+    print(previous_mic_value - int(mic_value/455) < 50)
+    print(previous_mic_value - int(mic_value/455))
+    
+    if previous_mic_value - int(mic_value/455) < 50:
+        print("hi")
+        previous_mic_value = int(mic_value/455)
+        color_value = int(mic_value/455)
+        COLOR = (color_value, 0, 0)
         pixels.fill(COLOR)
         pixels.show()
 
-    #print((color_value,))
-    #print(mic_value_array)
+    print((color_value,))
+    
+    # print(mic.value)
+    # time.sleep(1)
+    # print(mic_value_array)
